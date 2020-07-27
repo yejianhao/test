8d0dabb66a2de8a5d45727f0940c3d1042c3d6a0
叶剑豪 <jianhaoye20199@163.com>
2020-07-27 10:57:35 +0800

    添加新文件

---
+++ test-project/js/getData.js
@@ -0,0 +1,24 @@
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    };
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("../json/data.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+    // document.getElementById('title').innerText = data.title;
+    // document.getElementById('designScheme').innerText = data.designScheme;
+});
+
+
+
+
+
